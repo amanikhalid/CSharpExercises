6190b7985e5935de3e5cb286b39d30d378fc35d9
Amani Khalid <amanikhalidd@gmail.com>
2025-03-12 02:01:22 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿namespace CSharpExercise
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+
+            //    //1. Even or Odd
+
+            //    int num1;
+            //    Console.WriteLine("Enter a Number");
+            //    num1 = int.Parse(Console.ReadLine());
+
+            //    if (num1 % 2 == 0)
+            //    {
+            //        Console.WriteLine(num1 + "is an Even Number");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(num1 + "is an Odd Number");
+            //    }
+            ////-----------------------------------------------------------
+
+            //2. Largest of Three Numbers
+            //int num1, num2, num3, large;
+            //Console.WriteLine("Enter First Number:");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second Number");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Third Number");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 > num2 && num1 > num3)
+            //{
+            //    large = num1;
+            //}
+            //else if (num2 > num1 && num2 > num3)
+            //{
+            //    large = num2;
+            //}
+            //else
+            //{
+            //    large = num3;
+            //}
+            //Console.WriteLine("The Largest Number is " + large);
+            ////--------------------------------------------------------------
+
+            //3. Temperature Converter
+            //double temp, Fahrenheit;
+            //Console.WriteLine("Enter Temperature in Celsius");
+            //double Celsius = double.Parse(Console.ReadLine());
+
+            //Fahrenheit = (Celsius * 9 / 5) + 32;
+            //Console.WriteLine("Temperature in Fahrenheit is " + Fahrenheit); 
+
+            ////----------------------------------------------------------------
+
+            //4. Simple Discount Calculator
+            //float Price, FinalPrice, Discount;
+            //Console.WriteLine("Enter The Price");
+            //Price = float.Parse(Console.ReadLine());
+
+            //if (Price > 100)
+            //{
+            //    Discount = (Price / 100) *10;
+            //    FinalPrice = Price - Discount;
+            //    Console.WriteLine("Final Price is "+ FinalPrice);
+
+            //}
+            //else
+            //{
+            //    FinalPrice = Price;
+            //    Console.WriteLine("Final Price is "+ FinalPrice);
+            //}
+
+            ////-----------------------------------------------------------------
+
+            //5. Grading System
+            //float StudentMark;
+            //Console.WriteLine("Enter Your Score");
+            //StudentMark = float.Parse(Console.ReadLine());
+
+            //if (StudentMark >=90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (StudentMark >=80)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (StudentMark >=70)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (StudentMark >=60)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("F");
+            //}
+
+            ////---------------------------------------------------------------
+
+            //6. Swap Two Numbers
+            //int a, b,c;
+            //Console.WriteLine("Enter First Number");
+            //a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second Number");
+            //b = int.Parse(Console.ReadLine());
+
+            //c = a;
+            //a = b;
+            //b = c;
+            //Console.WriteLine("After Swapping First Number = " + a + ", Second Number = " + b);
+
+
+           //-------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
